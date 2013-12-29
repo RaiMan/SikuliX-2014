c31f80dd9d87f36bf42e0a3e884452a7adfd3b84
Raimund Hocke <rmhdevelop@me.com>
2013-12-29 13:48:14 +0100

    finally swathed to using the new OpenCV static lib libopencv_java247.dylib

--- Libs/build/mac/runBuildNatives.sh
+++ Libs/build/mac/runBuildNatives.sh
@@ -9,32 +9,23 @@ export DEVNATIVE=../../../Natives/src/main/native
 export DEVLIBS=../../src/main/resources/META-INF/libs/mac/libs$1
 
 echo ----- create JNI header for MacUtil
-#. runJNISetupForMacUtil.sh
+. runJNISetupForMacUtil.sh
 
 echo ----- build libMacUtil.dylib
-#. runBuildMacUtil.sh
+. runBuildMacUtil.sh
 
 echo ----- create JNI header for MacHotkeyManager
 #. runJNISetupForMacHotkeyManager.sh
 
 echo -----  build libMacHotkeyManager.dylib 
-#. runBuildMacHotkeyManager.sh
+. runBuildMacHotkeyManager.sh
 
 echo -----  use SWIG to create Java interface sources and native wrapper for VisionProxy
-#. runSwigForVision.sh
+. runSwigForVision.sh
 
 echo -----  build libVisionProxy.dylib 
 . runBuildVisionProxy.sh
 
 echo -----  switch external refs to point to @loader_path/
-# so the dependencies are loaded fom same folder as the Sikuli libs
-# not needed currently ./changeForAllToLoaderPath.sh $DEVLIBS libMacUtil.dylib
-#. changeForAllToLoaderPath.sh $DEVLIBS libMacHotkeyManager.dylib
-#cd $OLDPWD
-#. changeForAllToLoaderPath.sh $DEVLIBS libVisionProxy.dylib
-#cd $OLDPWD
-
-echo -----  change the external refs for OpenCV libs to major version string for @loader_path to work
-# this is a hack, that has to be checked for necessary changes 
-#. changeForVisionToLoaderPathSpecial.sh $DEVLIBS 2.4.6 2.4
-#cd $OLDPWD
+. changeForAllToLoaderPath.sh $DEVLIBS libVisionProxy.dylib
+cd $OLDPWD
