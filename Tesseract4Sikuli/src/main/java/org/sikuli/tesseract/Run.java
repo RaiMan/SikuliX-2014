1ff8c25f5e0960bb5ff4b1938b4d0cdc7e5ed806
Raimund Hocke <info@its-me-raiman.de>
2014-01-30 12:12:06 +0100

    integrated the relevant Tess4J stuff into the project (but not used yet) … more

--- Tesseract4Sikuli/src/main/java/org/sikuli/tesseract/Run.java
+++ Tesseract4Sikuli/src/main/java/org/sikuli/tesseract/Run.java
@@ -12,9 +12,10 @@ import org.sikuli.script.*;
 
 public class Run
 {
-		static String libPath = "/Users/rhocke/SikuliX/libs";
+		static String basePath = "/Users/rhocke/SikuliX";
+		static String libPath = basePath + "/libs";
 		static String datapath = libPath + "/tessdata";
-		static String testFile = "/eurotext.png";
+		static String testFile = "/tesstest.png";
     static String language = "eng";
     static TessAPI.TessBaseAPI handle;
 
@@ -30,20 +31,20 @@ public class Run
         TessAPI.TessBaseAPIInit3(handle, datapath, language);
 
 //				ScreenImage si = s.userCapture();
-        BufferedImage image = ImageIO.read(new File(libPath + testFile)); //si.getImage();
+        BufferedImage image = ImageIO.read(new File(basePath + testFile)); //si.getImage();
 
         ByteBuffer buf = Image.convertImageData(image);
         int bpp = image.getColorModel().getPixelSize();
         int bytespp = bpp / 8;
         int bytespl = (int) Math.ceil(image.getWidth() * bpp / 8.0);
 
-        TessAPI.TessBaseAPISetPageSegMode(handle, TessAPI.TessPageSegMode.PSM_AUTO);
+        TessAPI.TessBaseAPISetPageSegMode(handle, TessAPI.TessPageSegMode.PSM_SINGLE_LINE);
         TessAPI.TessBaseAPISetImage(handle, buf, image.getWidth(), image.getHeight(), bytespp, bytespl);
-        TessAPI.TessBaseAPISetRectangle(handle, 0, 0, 1024, 800);
+//        TessAPI.TessBaseAPISetRectangle(handle, 0, 0, 1024, 800);
         Pointer utf8Text = TessAPI.TessBaseAPIGetUTF8Text(handle);
 
 				String result = utf8Text.getString(0);
-        System.out.println("Tesseract returned: ]" + result + "[");
+        System.out.println(testFile + " returned: ***[\n" + result + "\n]***");
 
 				TessAPI.TessDeleteText(utf8Text);
         TessAPI.TessBaseAPIDelete(handle);
