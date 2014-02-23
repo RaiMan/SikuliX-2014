dedbf61f0b9f8d663dc88ccecd09784c349d8af9
Raimund Hocke <rmhdevelop@me.com>
2014-02-23 12:23:17 +0100

    added empty script language support classes (all service providers must be loadable at runtime) - should be changed in the long run to active loading of classes if more scripting support is to be added.

---
+++ JRubyFat/src/main/java/org/sikuli/idesupport/JythonIDESupport.java
@@ -0,0 +1,18 @@
+package org.sikuli.idesupport;
+
+import org.sikuli.basics.IDESupport;
+import org.sikuli.basics.IndentationLogic;
+
+public class JythonIDESupport implements IDESupport{
+
+  @Override
+  public String[] getEndings() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public IndentationLogic getIndentationLogic() {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+  
+}
