cbc31c4cd568d6e89bc9f1a9c752392623f5c227
Raimund Hocke <rmhdevelop@me.com>
2014-02-23 16:24:36 +0100

    collectjars.bat now tested on Windows - works

--- collectJars.bat
+++ collectJars.bat
@@ -1,32 +1,31 @@
 @echo off
 setlocal
 set mversion=1.1
-set sversion=%mversion%.0
+set version=%mversion%.0
 
 set base=%~dp0
-echo SourceBase %base%
 
-echo ----------------- SikuliX collecting jars in $base 
+echo ----------------- SikuliX collecting jars in %base% 
 
-set source=%base%/Setup/target
+set source=%base%Setup\target
 
-set dist=%base%/../SikuliX-Setup
-REM if [ -e $dist ]; then
-REM  rm -f -R $dist
-REM fi
-REM mkdir $dist 
-REM mkdir $dist/Downloads 
+set dist=%base%..\SikuliX-Setup
+if not exist "%dist%" goto NODIST
+rd /S /Q  %dist%
+:NODIST
+md %dist% 
+md %dist%\Downloads 
 
-set log=%dist%/collectjars-log.txt
+set log=%dist%\collectjars-log.txt
 
 echo --- version --- %version%
 echo --- major version --- %mversion%
-echo --- version --- %version% >>%log%
+echo --- version --- %version% >%log%
 echo --- major version --- %mversion% >>%log%
 
-echo ----------------- SourceBase $base >>%log%
+echo ----------------- SourceBase %base% >>%log%
 echo ----------------- running Maven clean install --- takes some time ...
-mvn clean install >>%log%
+call mvn clean install >>%log%
 
 REM ----------- Setup
 echo --- collecting jars
@@ -39,18 +38,18 @@ dir %source% >>%log%
 REM ----------- Setup
 echo --- copy Setup
 echo --- copy Setup >>%log%
-cp %source%/sikulixsetup*.jar %dist%/sikulixsetup-%mversion%.jar
-dir $dist >>$log
+copy %source%\sikulixsetup*.jar %dist%\sikulixsetup-%mversion%.jar >>%log%
+dir %dist% >>%log%
 
 REM ----------- Jars
 echo --- copy Jars 
 echo --- copy Jars >>%log%
-cp %source%/Downloads/* %dist%/Downloads
-dir %dist%/Downloads >>%log%
+copy %source%\Downloads\* %dist%\Downloads >>%log%
+dir %dist%\Downloads >>%log%
 
 echo ----------- final content of %dist%
-dir $dist
-dir $dist/Downloads
+dir %dist%
+dir %dist%\Downloads
 echo ----------- SikuliX collectjars end -----------
 
 :FINALLY
