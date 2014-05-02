3bd0ae4f28b33f373d24f55e26f9a7b28e24f947
Johan Sjöblom <sjoblomj@student.chalmers.se>
2014-05-02 20:46:24 +0200

    Correcting indentation

--- Tesseract4SikuliX/src/main/java/org/sikuli/tesseract/Run.java
+++ Tesseract4SikuliX/src/main/java/org/sikuli/tesseract/Run.java
@@ -12,41 +12,41 @@ import org.sikuli.script.*;
 
 public class Run
 {
-		static String basePath = "/Users/rhocke/SikuliX";
-		static String libPath = basePath + "/libs";
-		static String datapath = libPath + "/tessdata";
-		static String testFile = "/tesstest.png";
-    static String language = "eng";
-    static TessAPI.TessBaseAPI handle;
+  static String basePath = "/Users/rhocke/SikuliX";
+  static String libPath = basePath + "/libs";
+  static String datapath = libPath + "/tessdata";
+  static String testFile = "/tesstest.png";
+  static String language = "eng";
+  static TessAPI.TessBaseAPI handle;
 
-		public static void main( String[] args ) throws IOException
-    {
-				Debug.setDebugLevel(3);
-				Screen s = new Screen();
-				new Finder();
+  public static void main( String[] args ) throws IOException
+  {
+    Debug.setDebugLevel(3);
+    Screen s = new Screen();
+    new Finder();
 
-				System.setProperty("jna.encoding", "UTF8");
-				System.setProperty("jna.library.path", libPath);
-        handle = TessAPI.TessBaseAPICreate();
-        TessAPI.TessBaseAPIInit3(handle, datapath, language);
+    System.setProperty("jna.encoding", "UTF8");
+    System.setProperty("jna.library.path", libPath);
+    handle = TessAPI.TessBaseAPICreate();
+    TessAPI.TessBaseAPIInit3(handle, datapath, language);
 
-//				ScreenImage si = s.userCapture();
-        BufferedImage image = ImageIO.read(new File(basePath + testFile)); //si.getImage();
+    // ScreenImage si = s.userCapture();
+    BufferedImage image = ImageIO.read(new File(basePath + testFile)); //si.getImage();
 
-        ByteBuffer buf = Image.convertImageData(image);
-        int bpp = image.getColorModel().getPixelSize();
-        int bytespp = bpp / 8;
-        int bytespl = (int) Math.ceil(image.getWidth() * bpp / 8.0);
+    ByteBuffer buf = Image.convertImageData(image);
+    int bpp = image.getColorModel().getPixelSize();
+    int bytespp = bpp / 8;
+    int bytespl = (int) Math.ceil(image.getWidth() * bpp / 8.0);
 
-        TessAPI.TessBaseAPISetPageSegMode(handle, TessAPI.TessPageSegMode.PSM_AUTO);
-        TessAPI.TessBaseAPISetImage(handle, buf, image.getWidth(), image.getHeight(), bytespp, bytespl);
-//        TessAPI.TessBaseAPISetRectangle(handle, 0, 0, 1024, 800);
-        Pointer utf8Text = TessAPI.TessBaseAPIGetUTF8Text(handle);
+    TessAPI.TessBaseAPISetPageSegMode(handle, TessAPI.TessPageSegMode.PSM_AUTO);
+    TessAPI.TessBaseAPISetImage(handle, buf, image.getWidth(), image.getHeight(), bytespp, bytespl);
+    // TessAPI.TessBaseAPISetRectangle(handle, 0, 0, 1024, 800);
+    Pointer utf8Text = TessAPI.TessBaseAPIGetUTF8Text(handle);
 
-				String result = utf8Text.getString(0);
-        System.out.println(testFile + " returned: ***[\n" + result + "\n]***");
+    String result = utf8Text.getString(0);
+    System.out.println(testFile + " returned: ***[\n" + result + "\n]***");
 
-				TessAPI.TessDeleteText(utf8Text);
-        TessAPI.TessBaseAPIDelete(handle);
-    }
+    TessAPI.TessDeleteText(utf8Text);
+    TessAPI.TessBaseAPIDelete(handle);
+  }
 }
