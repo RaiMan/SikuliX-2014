5f22b9136736550bd911a78fd92434da846a91bb
Raimund Hocke <rmhdevelop@me.com>
2014-06-16 17:37:00 +0200

    minor

--- API/src/main/java/org/sikuli/script/OverlayTransparentWindow.java
+++ API/src/main/java/org/sikuli/script/OverlayTransparentWindow.java
@@ -109,6 +109,7 @@ public class OverlayTransparentWindow extends JFrame implements EventSubject {
     _obs.update(this);
   }
 
+  @Override
   public void setOpacity(float alpha) {
     if (Settings.JavaVersion > 6) {
       try {
