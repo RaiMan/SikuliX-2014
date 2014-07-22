3a4304c324c9587f6f32d21f7d1403589b9c697d
Raimund Hocke <info@its-me-raiman.de>
2014-07-22 14:29:08 +0200

    added a an example how to use the artifacts from OSSRH in a Maven project

---
+++ TestRunMaven/src/main/java/com/sikulix/testrun/TestRun.java
@@ -0,0 +1,14 @@
+package com.sikulix.testrun;
+
+import org.sikuli.script.*;
+import org.sikuli.basics.Debug;
+
+public class TestRun
+{
+    public static void main( String[] args )
+    {
+        Debug.test( "SikuliX 2014 TestRun: hello" );
+				Debug.setDebugLevel(3);
+				Sikulix.init();
+    }
+}
