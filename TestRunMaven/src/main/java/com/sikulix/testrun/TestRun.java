fdad789e12874df0ea337192c5524280726e4f0e
Raimund Hocke <info@its-me-raiman.de>
2014-08-01 10:53:59 +0200

    testing new debug redirect features

--- TestRunMaven/src/main/java/com/sikulix/testrun/TestRun.java
+++ TestRunMaven/src/main/java/com/sikulix/testrun/TestRun.java
@@ -9,6 +9,19 @@ public class TestRun
     {
         Debug.test( "SikuliX 2014 TestRun: hello" );
 				Debug.setDebugLevel(3);
-				Sikulix.init();
+				//Sikulix.init();
+				Debug.test("setting logger");
+				Debug.setLogger(new TestRun());
+				Debug.test("setting logger all redirect");
+				Debug.setLoggerAll("info");
+				Debug.test("testing redirection");
+				Debug.info("test redirection info");
+				Debug.action("test redirection info");
+				Debug.error("test redirection info");
+        Debug.test( "SikuliX 2014 TestRun: end" );
     }
+
+	public void info(String msg) {
+		System.out.println("[TEST] myLogger.info: " + msg);
+	}
 }
