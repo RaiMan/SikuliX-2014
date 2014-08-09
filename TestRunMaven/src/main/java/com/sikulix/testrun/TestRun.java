d9b7fc36ff735777f21249291d8a9e5e24b9c11c
Raimund Hocke <info@its-me-raiman.de>
2014-08-09 12:21:44 +0200

    minor

--- TestRunMaven/src/main/java/com/sikulix/testrun/TestRun.java
+++ TestRunMaven/src/main/java/com/sikulix/testrun/TestRun.java
@@ -11,20 +11,26 @@ public class TestRun {
 
   public static void main(String[] args) throws FindFailed, IOException {
     Debug.test("SikuliX 2014 TestRun: hello");
-    Debug.setDebugLevel(0);
-    Screen s = Sikulix.init();
-    //TextRecognizer.getInstance();
-		String imgN = "./target/classes/images/images.sikuli/image.png";
-		URL dir = FileManager.makeURL("./target/classes/images/images.sikuli");
-		Debug.test("URL: %s", FileManager.makeURL("./target/classes/images/images.sikuli"));
     Debug.setDebugLevel(3);
-		Debug.test("***** 1st image");
-		Image img = Image.create(imgN);
-		Debug.test("***** 2nd image");
-		img = Image.create(imgN);
-		Image.dump();
-		Debug.test("***** purge");
-		Image.purge(dir);
+    Screen s = Sikulix.init();
+
+		//TextRecognizer.getInstance();
+
+//		String imgN = "./target/classes/images/images.sikuli/image.png";
+//		URL dir = FileManager.makeURL("./target/classes/images/images.sikuli");
+//		Debug.test("URL: %s", FileManager.makeURL("./target/classes/images/images.sikuli"));
+//    Debug.setDebugLevel(3);
+//		Debug.test("***** 1st image");
+//		Image img = Image.create(imgN);
+//		Debug.test("***** 2nd image");
+//		img = Image.create(imgN);
+//		Image.dump();
+//		Debug.test("***** purge");
+//		Image.purge(dir);
+
+    Debug.setDebugLevel(3);
+		Debug.log(3, "trying testSetup");
+		Debug.test("testSetup: #returned#", Sikulix.testSetup());
 		System.exit(1);
   }
 }
