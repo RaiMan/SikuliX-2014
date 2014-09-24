4955da41ed548f1c61d6dc4509562660629d33b6
Raimund Hocke <info@its-me-raiman.de>
2014-09-24 14:21:58 +0200

    test script

---
+++ StuffContainer/testScripts/testRegionParts/testParts.sikuli/testParts.py
@@ -0,0 +1,27 @@
+# 1-Pixel column
+r = get(Region.MM)
+r.getCol(0, r.w+1).highlight(1)
+
+get(Region.NH).highlight(1)
+get(Region.MH).highlight(1)
+get(Region.SH).highlight(1)
+get(Region.WH).highlight(1)
+get(Region.MV).highlight(1)
+get(Region.EH).highlight(1)
+
+get(Region.TT).highlight(1)
+get(Region.RR).highlight(1)
+get(Region.LL).highlight(1)
+get(Region.BB).highlight(1)
+get(Region.M2).highlight(1)
+
+get(Region.NW).highlight(1)
+get(Region.NM).highlight(1)
+get(Region.NE).highlight(1)
+get(Region.WM).highlight(1)
+get(Region.MM).highlight(1)
+get(Region.EM).highlight(1)
+get(Region.SW).highlight(1)
+get(Region.SM).highlight(1)
+get(Region.SE).highlight(1)
+
