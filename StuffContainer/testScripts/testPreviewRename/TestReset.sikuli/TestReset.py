4525affff436545eb548f12af698320ca907ae06
Raimund Hocke <rmhdevelop@me.com>
2014-10-05 10:25:33 +0200

    test script

--- StuffContainer/testScripts/testPreviewRename/TestReset.sikuli/TestReset.py
+++ StuffContainer/testScripts/testPreviewRename/TestReset.sikuli/TestReset.py
@@ -1,2 +1,8 @@
+print "Cache: ", Settings.getImageCache()
+#Settings.setImageCache(64)
+#print "Cache: ", Settings.getImageCache()
+Image.dump()
 "img1.png"
 "img1.png"
+for i in range(3):
+  find("1412406181306.png")
