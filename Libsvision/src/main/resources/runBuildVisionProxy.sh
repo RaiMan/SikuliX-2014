b6f5dc2328bb1353782c217ad5b2ebdba3d0bc02
Raimund Hocke <info@its-me-raiman.de>
2014-12-09 14:49:59 +0100

    started: one-script version of Linux build libVisionProxy

--- Libsvision/src/main/resources/runBuildVisionProxy.sh
+++ Libsvision/src/main/resources/runBuildVisionProxy.sh
@@ -1,12 +1,72 @@
 #!/bin/sh
 # this script compiles and links a Sikuli module on Linux 32 or 64Bit
 
-# variable DEVNATIVE is set outside - the native source files
-# variable JDK is set outside - Java JDK
-# variable LIBS is set outside - link libraries
-
 # abort on the first error
 set -e
+
+if [ -z $1 ]; then
+  ARCH=`getconf LONG_BIT`
+else
+  ARCH=$1
+fi
+
+if ! [[ "$ARCH" == "32" || "$ARCH" == "64" ]]; then
+  printf  "Usage: $0 [ 32 | 64 ]\nSpecify 32 or 64 bit\n"
+  exit
+fi
+
+DEVJAVA=../../../../API/src/main/java
+DEVNATIVE=../../../../Libsvision/src/main/java/native
+DEVLIBS=../../../../Libslux/src/main/resources/META-INF/libs/linux/libs$ARCH/
+
+echo -----  Linux build workflow for native modules on $ARCH-bit systems
+
+# trying to find the active JDK
+if [ -z "$JDK" ]; then
+	for e in `whereis -b javac`; do
+		if [ "$e" == "javac:" ]; then
+			continue
+		else
+			jvc=$e
+			break
+		fi
+	done
+
+	jvcx=
+	while [ "$jvc" != "" ]; do
+		jvcx=$jvc
+		jvc=`readlink -n $jvc`
+	done
+
+	JDK=
+	if [ "$jvcx" != "" ]; then
+		JDK=`dirname $jvcx`
+		JDK=`dirname $JDK`
+	fi
+fi
+
+if [ -e $JDK/include/jni.h ]; then
+  # folder(s) containing the header files
+  echo --- The active JDK seems to be in $JDK
+else
+  echo --- JDK could not be found - please set the \"JDK\" environment variable
+fi
+
+libsOpenCV=
+if pkg-config opencv; then
+  echo --- OpenCV version $(pkg-config --modversion opencv) found
+  libsOpenCV=$(pkg-config --libs opencv)
+else
+  echo --- OpenCV libs could not be found
+fi
+
+libsTesseract=
+if pkg-config tesseract; then
+  echo --- Tesseract version $(pkg-config --modversion tesseract) found
+  libsTesseract=$(pkg-config --libs tesseract)
+else
+  echo --- Tesseract libs could not be found
+fi
 
 # native Sikuli sources
 src=$DEVNATIVE
@@ -39,18 +99,28 @@ done
 
 echo -- finally linking
 rm -f libVisionProxy.so
-link_str+="/usr/lib/x86_64-linux-gnu/libopencv_core.so "
-link_str+="/usr/lib/x86_64-linux-gnu/libopencv_highgui.so "
-link_str+="/usr/lib/x86_64-linux-gnu/libopencv_imgproc.so "
-link_str+="/usr/lib/libtesseract.so "
-echo $link_str
-g++ $link_str 
+
+if [ "" == "libsOpenCV" ]; then
+	link_str+="/usr/lib/x86_64-linux-gnu/libopencv_core.so "
+	link_str+="/usr/lib/x86_64-linux-gnu/libopencv_highgui.so "
+	link_str+="/usr/lib/x86_64-linux-gnu/libopencv_imgproc.so "
+else
+	link_str+=$libsOpenCV
+fi
+
+if [ "" == "libsTesseract" ]; then
+	link_str+="/usr/lib/libtesseract.so "
+else
+	link_str+=$libsTesseract
+fi
+
+g++ $link_str
 
 if [ -e libVisionProxy.so ]; then
   echo -- checking created libVisionProxy.so
   undefined=`ldd -r libVisionProxy.so | grep -c "undefined symbol:"`
   if [ "undefined" == "0" ]; then
-    if [ -e $DEVLIBS ]; then    
+    if [ -e $DEVLIBS ]; then
       cp libVisionProxy.so $DEVLIBS
     fi
     echo -- should be useable
