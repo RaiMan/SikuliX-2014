5e4ef8b03be3652e54e3d14bb5ae092dc91a3dcc
Raimund Hocke <info@its-me-raiman.de>
2015-03-06 19:09:49 +0100

    continued: support for JavaScript

--- StuffContainer/testScripts/testJavaScript/testCommands/testWait/testWait.js
+++ StuffContainer/testScripts/testJavaScript/testCommands/testWait/testWait.js
@@ -1,31 +1,27 @@
 main = function() {
   image = "nbicons";
-  Debug.info("exists(image, 0, 99)");
   match = exists(image, 0, 99);
+	println(match);
   if (isNull(match)) {
     Debug.error("exists: not found");
     return;
   }
-  Debug.info("wait(new Pattern(image).similar(0.8), 0.0)");
   match = wait(new Pattern(image).similar(0.8), 0.0);
-  hover(fromJSON(match), 300, 300);
-  Debug.info("hover(image, 0, 99)");
+	println(match);
+  hover(match, 300, 300);
   hover(image, 0, 99);
-	jsonOff();
   loc = doubleClick(0, 50);
   while (loc.x < 500) {
     loc = hover(loc, 100, 30);
   }
-	jsonOn();
-  println(rightClick(0,50));
+  println("rightClick: " + rightClick(0,50));
 	return;
-}
+};
 
 Debug.on(2);
 Debug.user("Starting test");
-jsonOff();
-//use();
-//println(use(Region.create(500,0,300,300)))
+use();
+//use(Region.create(500,0,300,300));
 main();
 Debug.user("Ending test");
 
