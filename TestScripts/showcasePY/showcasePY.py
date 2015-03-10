68f7fb9584719c790844624f2f74f5d07ee4c860
Raimund Hocke <info@its-me-raiman.de>
2015-03-10 14:54:24 +0100

    showcase

--- TestScripts/showcasePY/showcasePY.py
+++ TestScripts/showcasePY/showcasePY.py
@@ -1,1 +1,23 @@
-popup("Hello from Git showcasePY");
+popup("Hello from Git showcase Jython")
+
+import java.awt.Desktop as DT
+import java.net.URI as URI
+
+if not DT.isDesktopSupported():
+	exit(1)
+
+dt = DT.getDesktop()
+dt.browse(URI("http://sikulix.com"))
+
+r = wait("sxpower", 5)
+use(r.below(100))
+click("quickstart")
+use()
+wait(3)
+if RunTime.get().runningMac:
+  write("#M.w")
+else:
+  write("#C.w")
+
+
+
