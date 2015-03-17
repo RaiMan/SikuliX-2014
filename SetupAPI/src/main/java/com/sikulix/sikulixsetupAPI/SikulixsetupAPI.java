2fdfa41c8711147fe683a9abc535f35a41d05e41
Raimund Hocke <info@its-me-raiman.de>
2015-03-17 09:53:48 +0100

    more preps for Maven upload

---
+++ SetupAPI/src/main/java/com/sikulix/sikulixsetupAPI/SikulixsetupAPI.java
@@ -0,0 +1,9 @@
+package com.sikulix.sikulixsetupAPI;
+
+/**
+ * This package is not intended for usage in Maven projects<br>
+ * It can/will be downloaded to be used with sikulixsetup
+ */
+public class SikulixsetupAPI {
+
+}
