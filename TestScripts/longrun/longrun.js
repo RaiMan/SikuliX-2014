1c56ba3dc411fbb71c3deae500450a833337f857
Raimund Hocke <rmhdevelop@me.com>
2015-03-19 16:55:15 +0100

    test script longrun

--- TestScripts/longrun/longrun.js
+++ TestScripts/longrun/longrun.js
@@ -1,3 +1,13 @@
+whenFix = function(img, reg, timeout) {
+  m = null;
+  while(true) {
+    m = reg.wait(img, timeout);
+    wait(0.5);
+    if (!isNull(m.exists(img, 0))) break;
+  }
+  return m;
+}
+
 main = function() {
   link = "http://sikulix.com";
   worked = App.openLink(link);
@@ -10,31 +20,30 @@ main = function() {
     Debug.error("App.openLink(%s): did not open", link);
     return false;
   }
-	use1(r.below(100));
-	click("stories");
+	r.below(100).click("stories");
 
 	win = App.focusedWindow();
-  use(win)
   
-	click("knowhow");
-	click("topic");
+	click(win.wait("knowhow", 10.0));
+	click(win.wait("topic", 10.0));
+
   r = win.getLastMatch().below(1).below(350).grow(0, 300, 0, 0)
-  r.highlight(4);
-	r.click("reply");
-  click("form");
+	r.click(whenFix("reply", r, 5.0));
+  
+  click(whenFix("form", win, 5.0));
   
   type("Raimund Hocke" + Key.TAB + Key.TAB + Key.TAB)
 	type("Now that I am here, I can go back and start again")
   wait(2);
 	closeBrowserWindow();
-  win.above(1).below(300).click("close");
+  click(whenFix("close", win.above(1).below(300), 5.0));
   wait(3);
 };
 
 torun = Sikulix.input("Hello from git showcase JavaScript: longrun\n" +
          "How often should it be run? (per run about 20 secs)", "1");
 RUNTIME.setOption("forJS", torun);
-max = RUNTIME.getOptionNumber("forJS", 10);
+max = RUNTIME.getOptionNumber("forJS", 1);
 for (i = 0; i < max; i++) {
   main();
 }
