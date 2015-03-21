a057d5f54261fd2bef610f3369fbf0905da4cbed
raiman <raiman@raimanubu64>
2015-03-21 15:57:18 +0100

    showcase

--- TestScripts/showcase/showcase.js
+++ TestScripts/showcase/showcase.js
@@ -5,13 +5,13 @@ main = function() {
     Debug.error("App.openLink(%s): did not work", link);
     return false;
   }
-  r = wait("sxpower", 5);
+  r = wait("sxpower", 30);
   if (isNull(r)) {
     Debug.error("App.openLink(%s): did not open", link);
     return false;
   }
   use1(r.below(100));
-  click("quickstart");
+  click(wait("quickstart", 30));
   wait(3);
   closeBrowserWindow();
 };
