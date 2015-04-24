beadeacc2fb6980dfb0a7e369a887e440319a8b3
Raimund Hocke <rmhdevelop@me.com>
2015-04-24 09:40:32 +0200

    testscript

--- StuffContainer/testScripts/testOSX1010/testNotes.sikuli/testNotes.py
+++ StuffContainer/testScripts/testOSX1010/testNotes.sikuli/testNotes.py
@@ -1,13 +1,14 @@
 Debug.on(3)
-Debug.off()
+#Debug.off()
 App.open("Notes")
+#runScript('applescript tell app "Notes" to activate') 
 btn1 = Pattern("btn1.png").targetOffset(-19,30)
 btn2 = "btn2.png"
 wait("AllNotes.png", 10)
 reg = App.focusedWindow()
 reg.highlight(1)
 
-loopSave = 15
+loopSave = 60
 start = time.time()
 for i in range(50):
   loop1 = time.time()
@@ -17,9 +18,9 @@ for i in range(50):
   if (time.time()-loop1 < loopSave): 
     click(btn1)
   if (time.time()-loop1 < loopSave): 
-    type("%d %f" % (i, time.time()-loop1))
+    type("%d %.1f %d" % (i, time.time()-loop1, int(time.time()-start)))
     type(Key.ENTER, Key.ALT)
-    wait(1)
+    wait(50)
   loop2 = time.time()-loop1
   if (loop2 > loopSave * 2): 
     print "slowdown at %d after %f secs" % (i, time.time() - start)
