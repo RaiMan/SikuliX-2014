28fcb5a851d9c9fd0a0248341b3d6996da69b307
Raimund Hocke <rmhdevelop@me.com>
2015-04-30 11:54:17 +0200

    minor

--- IDE/src/main/java/org/sikuli/ide/SikuliIDE.java
+++ IDE/src/main/java/org/sikuli/ide/SikuliIDE.java
@@ -19,7 +19,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
