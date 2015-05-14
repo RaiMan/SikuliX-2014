4b2185ac14c693f2194cfdb1ca8b52c0273a6df6
Raimund Hocke <rmhdevelop@me.com>
2015-05-14 16:44:45 +0200

    test script

--- StuffContainer/testScripts/testMulti/test.sikuli/test.py
+++ StuffContainer/testScripts/testMulti/test.sikuli/test.py
@@ -3,23 +3,24 @@ s0 = Screen(0)
 Debug.logp("0: %s", s0)
 s1 = Screen(1)
 Debug.logp("1: %s", s1)
-"""
+
 s0.hover()
 wait(2)
 s1.hover()
 wait(2)
 s0.hover()
-"""
-bsw = App("Safari").open().window()
-bsw.highlight(1)
+
 img = "img.png"
 #Debug.on(4)
 m = s1.find(img)
 hover(m)
 hover(s1)
 saveCapture("test")
-m = bsw.find("_test.png")
+m = s1.find("_test.png")
 hover(m)
 
 icons = "icons.png"
 hover(icons)
+
+
+
