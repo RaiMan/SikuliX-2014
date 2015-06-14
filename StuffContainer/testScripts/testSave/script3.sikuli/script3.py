d0a2ccda100b1897e228956d1bf1e3119e505765
Raimund Hocke <rmhdevelop@me.com>
2015-06-14 12:36:29 +0200

    revising: App class: intermediate

--- StuffContainer/testScripts/testSave/script3.sikuli/script3.py
+++ StuffContainer/testScripts/testSave/script3.sikuli/script3.py
@@ -1,14 +1,23 @@
 iePath = r'"C:\Program Files\Internet Explorer\iexplore.exe" '
 ieLink = "http://germany.mfa.gov.ua/ua"
-title = ucode("Посольство України")
 title = "powered by"
+
+iePath = "C:\Python27\python.exe"
+
+ie = App.open(iePath)
+wait(3)
+#click()
+#wait(1)
+#ie.focus()
+#wait(2)
+ie.close()
+
+exit()
 
 click()
 wait(1)
 ie = App.focus(title)
 wait(2)
-#print ie
-exit()
 
 ie = App.open(iePath + ieLink)
 
@@ -16,9 +25,8 @@ wait(3)
 click()
 wait(2)
 
-App.focus("SikuliX powered")
+App.focus(title)
 wait(2)
-
 
 wait(3)
 click()
@@ -26,3 +34,5 @@ wait(2)
 
 ie.focus()
 wait(2)
+
+ie.close()
