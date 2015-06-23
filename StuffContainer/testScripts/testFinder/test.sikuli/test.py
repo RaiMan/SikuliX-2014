6d6d1aef96f928dc510ab82718c711fe91a68634
Raimund Hocke <rmhdevelop@me.com>
2015-06-23 11:21:39 +0200

    test script

--- StuffContainer/testScripts/testFinder/test.sikuli/test.py
+++ StuffContainer/testScripts/testFinder/test.sikuli/test.py
@@ -7,8 +7,10 @@ m = find(imgBase)
 print "*** Finder same"
 f = Finder(imgBase)
 print f.find(imgBase)
+print f.hasNext()
 print "same", f.next()
 
 print "*** Finder contained"
 f.find(imgTarget)
+print f.hasNext()
 print "contained", f.next()
