3b4be7465cc11d43854c8d3a6dd755aced550f82
Raimund Hocke <info@its-me-raiman.de>
2015-06-24 11:02:13 +0200

    test script

---
+++ StuffContainer/testScripts/testVisualAnnotation/testHighlight.sikuli/testHighlight.py
@@ -0,0 +1,7 @@
+import org.sikuli.util.ScreenHighlighter as SH
+Region(100, 100, 100, 100).highlight(); wait(1)
+popup("ok")
+Region(100, 300, 100, 100).highlight(); wait(1)
+popup("ok2")
+SH.closeAll()
+wait(3)
